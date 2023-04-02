ebbde2ad80175e907cd66b7d58e38dbce7b667c5
chekm <chekmenevkirill@gmail.com>
2023-04-02 19:00:44 +0300

    HashStorage dApps

--- InGetTransact.py
+++ InGetTransact.py
@@ -7,17 +7,16 @@ WEB3 = Web3(HTTPProvider(BLOCKCHAIN_ADDRESS))
 COMPILED_CONTRACT_PATH = 'build/contracts/HashStorage.json'
 DEPLOYED_CONTRACT_ADDRESS = '0x02B87CB8eb20d755cD91D9Cc546637b41F5b359A'
 
+with open(COMPILED_CONTRACT_PATH) as file:
+    contractJson = json.load(file)
+    contractAbi = contractJson['abi']
+
+contract = WEB3.eth.contract(address=DEPLOYED_CONTRACT_ADDRESS, abi=contractAbi)
+
 
 class Transact:
 
     def getTransaction(self, _address, _id):
-
-        with open(COMPILED_CONTRACT_PATH) as file:
-            contractJson = json.load(file)
-
-            contractAbi = contractJson['abi']
-
-        contract = WEB3.eth.contract(address=DEPLOYED_CONTRACT_ADDRESS, abi=contractAbi)
         senderAddress = _address
         output = contract.functions.getHash(_id).call({'from': WEB3.to_checksum_address(senderAddress)})
 
@@ -25,12 +24,6 @@ class Transact:
 
     def insertTransaction(self, _address, _id, _hash):
 
-        with open(COMPILED_CONTRACT_PATH) as file:
-            contractJson = json.load(file)
-
-            contractAbi = contractJson['abi']
-
-        contract = WEB3.eth.contract(address=DEPLOYED_CONTRACT_ADDRESS, abi=contractAbi)
         senderAddress = _address
         txHash = contract.functions.insertHash(_id, _hash).transact({'from': WEB3.to_checksum_address(senderAddress)})
         txReceipt = WEB3.eth.wait_for_transaction_receipt(txHash)
