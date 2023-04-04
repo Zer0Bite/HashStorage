7026863ebd351d13968247bf154b48592ea2c47d
chekm <chekmenevkirill@gmail.com>
2023-04-04 13:06:42 +0300

    HashStorage dApps

--- InGetTransact.py
+++ InGetTransact.py
@@ -1,12 +1,11 @@
 import json
 from web3 import Web3, HTTPProvider
 
-print('Enter contract address')
 
-BLOCKCHAIN_ADDRESS = 'http://127.0.0.1:9545'
+BLOCKCHAIN_ADDRESS = 'http://127.0.0.1:7545'
 WEB3 = Web3(HTTPProvider(BLOCKCHAIN_ADDRESS))
 COMPILED_CONTRACT_PATH = 'build/contracts/HashStorage.json'
-DEPLOYED_CONTRACT_ADDRESS = 'smart-contract address'
+DEPLOYED_CONTRACT_ADDRESS = '0x90f4eab8D774d611d18c31e1689d97f733C49114'
 
 with open(COMPILED_CONTRACT_PATH) as file:
     CONTRACT_JSON = json.load(file)
