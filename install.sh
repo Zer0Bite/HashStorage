7ced553878082a5489ce830223ee9b7a6c3dafb4
chekm <chekmenevkirill@gmail.com>
2023-04-06 12:59:47 +0300

    HashStorage dApps

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install nodejs
 sudo apt install npm
 sudo npm install -g truffle
+sudo apt -y install python3-pip
 sudo pip install web3==6.0.0
 sudo pip install flask==2.2.2
 sudo pip install pyfiglet
