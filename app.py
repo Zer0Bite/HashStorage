9a67a3a9688619f92a483cf9bbc6610ab86d2cde
chekm <chekmenevkirill@gmail.com>
2023-04-24 15:49:32 +0300

    HashStorage dApps

--- app.py
+++ app.py
@@ -1,64 +1,73 @@
+import datetime
 import random
-from flask import Flask, request, jsonify, render_template, session, send_file, redirect
+import os
+import uuid
+import pyfiglet
+from flask import Flask, request, jsonify, render_template, send_file
 from pathlib import Path
 import InGetTransact
 import hash
-import pyfiglet
 
-intro = pyfiglet.figlet_format('Hash Storage', font = 'slant')
+intro = pyfiglet.figlet_format('Hash Storage', font='slant')
 print('-------------------------------------------------------------------------')
 print(intro)
 print('-------------------------------------------------------------------------')
 
-app = Flask(__name__,  template_folder='template', static_folder='static')
+app = Flask(__name__, template_folder='template', static_folder='static')
 
 
 @app.route('/')
-def Index():
+def index():
     return render_template('index.html')
 
 
 @app.route('/hash')
-def IndexHash():
+def index_hash():
     return render_template('hash.html')
 
 
-@app.route('/hash', methods=['GET', 'POST'])
-def UploadHash():
+@app.route('/hash', methods=['POST'])
+def upload_hash():
     file = request.files['file']
     file.save(file.filename)
-    fileHash = hash.getHash(file.filename)
-    userAddress = request.form['userAddress']
-    userId = random.getrandbits(212)
-    with open('certificate\\certificate.txt', "w") as fileCertificate:
-        fileCertificate.write('Address: ' + str(userAddress) + '\n' + 'File Name: ' + file.filename + '\n' + 'ID: ' + str(userId) + '\n' + 'File Hash: ' + fileHash)
+    file_hash = hash.getHash(file.filename)
+    user_address = request.form['userAddress']
+    user_id = uuid.uuid4().int
+    print(user_id)
+    certificate_path = os.path.join('certificate', file.filename + '.txt')
+    with open(certificate_path, "w") as certificate_file:
+        certificate_file.write('Address: ' + str(user_address) + '\n' +
+                                'File Name: ' + file.filename + '\n' +
+                                'ID: ' + str(user_id) + '\n' +
+                                'File Hash: ' + file_hash + '\n' +
+                                'Date: ' + str(datetime.datetime.now()))
     transact = InGetTransact.Transact()
-    transact.insertTransaction(str(userAddress), int(userId), str(fileHash))
-    path = 'certificate\\certificate.txt'
+    transact.insertTransaction(str(user_address), int(user_id), str(file_hash))
     Path(file.filename).unlink()
-    return send_file(path, as_attachment=True)
+    return send_file(certificate_path, as_attachment=True)
+
 
 
 @app.route('/verify')
-def VerifyIndex():
+def verify_index():
     return render_template('verify.html')
 
 
 @app.route('/verify', methods=['POST'])
-def Verify():
-    userAddress = request.form['userAddress']
-    userId = request.form['userId']
+def verify():
+    user_address = request.form['userAddress']
+    user_id = request.form['userId']
     file = request.files['file']
     file.save(file.filename)
-    fileHash = hash.getHash(file.filename)
+    file_hash = hash.getHash(file.filename)
     transact = InGetTransact.Transact()
-    hashInBlock = transact.getTransaction(str(userAddress), int(userId))
+    hash_in_block = transact.getTransaction(str(user_address), int(user_id))
     Path(file.filename).unlink()
-    if fileHash == hashInBlock:
+    if file_hash == hash_in_block:
         return render_template('verify.html', show_panel_good=True)
     else:
         return render_template('verify.html', show_panel_bad=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
